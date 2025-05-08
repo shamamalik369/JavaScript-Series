f9f97a16b21a2bc03393853c23c9733ab675dc6a
Shama Malik <73518502+shamamalik369@users.noreply.github.com>
2025-05-08 06:30:25 +0000

    Number math concept

---
+++ o1_basic/number_math.js
@@ -0,0 +1,34 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(1));
+
+const otherNumber = 123.8966
+
+//console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+
+//+++++++++++++++++++++++++++++++++ MATH +++++++++++++++++++++++++++++++++++++++++++++
+
+console.log(Math);
+//console.log(Math.abs(-6));
+//console.log(Math.round(4.7));
+//console.log(Math.floor(5.6));
+//console.log(Math.min(2,5,7,9,1));
+//console.log(Math.max(2,5,7,9,1));
+
+
+console.log(Math.random());
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
